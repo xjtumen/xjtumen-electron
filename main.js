88e3b47be5310a060eac5dcfce105f6fe665a63c
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2024-05-02 12:32:06 +0800

    v0.0.1 release

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const {app, BrowserWindow} = require('electron');
-
-let mainWindow;
+const path = require('path');
+const spawn = require('child_process').spawn;
 
 app.on('window-all-closed', function() {
   app.quit();
 });
 
 app.on('ready', function() {
+  const binaryFileName = process.platform == 'win32' ? 'xmentccnbg.exe' : 'xmentccnbg';
+  const binaryFile = path.join(process.resourcesPath, `bin/${binaryFileName}`);
+  spawn(binaryFile);
+
   let mainWindow = new BrowserWindow({width: 1024, height: 768, webPreferences: {nodeIntegration: true, webviewTag: true} });
   mainWindow.webContents.session.setProxy({proxyRules:"socks5://127.0.0.1:2588"});
 
